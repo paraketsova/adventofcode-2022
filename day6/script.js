ccc33b8d39d874d868fb40030a8f44672928e0b8
Mariia Paraketsova <paraketsova@gmail.com>
2022-12-11 18:01:50 +0100

    Day 6 Part 2: Tuning Trouble

--- day6/script.js
+++ day6/script.js
@@ -5,10 +5,21 @@ const data = fs.readFileSync("input-6.txt", { encoding: "utf8" });
 console.log(data);
 let markers = [];
 // create sub array begin with every letter
-for (let i = 0; i < data.length - 3; i++) {
+//for Part one:
+// for (let i = 0; i < data.length - 3; i++) {
+//   let marker = [];
+//   // let marker = [data[i], data[i + 1], data[i + 2], data[i + 3]];
+//   for (let j = 0; j <= 3; j++) {
+//     marker.push(data[i + j]);
+//   }
+//   markers.push(marker);
+// }
+
+//for Part two:
+for (let i = 0; i < data.length - 13; i++) {
   let marker = [];
   // let marker = [data[i], data[i + 1], data[i + 2], data[i + 3]];
-  for (let j = 0; j <= 3; j++) {
+  for (let j = 0; j <= 13; j++) {
     marker.push(data[i + j]);
   }
   markers.push(marker);
@@ -16,6 +27,33 @@ for (let i = 0; i < data.length - 3; i++) {
 console.log(markers);
 
 // PROCESSING =====================
+
+// Part One
+// const detectMarker = (markers) => {
+//   let result = 0;
+//   markers.find((marker) => {
+//     // check subarray if it have only uniq elements
+//     const duplicates = marker.filter((letter, index, marker) => {
+//       // console.log(letter); // letter - element of subarray
+//       // console.log(index); // index - index of element
+//       // console.log(marker); // marker - subarray
+//       return marker.indexOf(letter) !== index;
+//     });
+//
+//     // return markers.indexOf(marker);
+//     if (duplicates.length === 0) {
+//       console.log(markers.indexOf(marker));
+//       console.log(marker);
+//       result = markers.indexOf(marker);
+//       return true;
+//     }
+//   });
+//   return result + 4;
+// };
+//
+// console.log(detectMarker(markers));
+
+// Part Two
 const detectMarker = (markers) => {
   let result = 0;
   markers.find((marker) => {
@@ -35,7 +73,7 @@ const detectMarker = (markers) => {
       return true;
     }
   });
-  return result + 4;
+  return result + 14;
 };
 
 console.log(detectMarker(markers));
